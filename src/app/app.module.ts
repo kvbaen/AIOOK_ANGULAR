386f826d98adc58a4900e23d54f7d332fc8f138b
Jakub Malinowski <jjakub.m.00@gmail.com>
2022-01-16 14:56:58 +0100

    ng new

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { environment } from '../environments/environment';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFireAuthModule} from '@angular/fire/compat/auth';
+import { MoviesComponent } from './components/movies/movies.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MoviesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule
+  ],
+  providers: [
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'aiook-ang',
+    appId: '1:182774739783:web:7c4bbfa5778b0ae4a31262',
+    storageBucket: 'aiook-ang.appspot.com',
+    apiKey: 'AIzaSyCCm_WL7g7LHJ9RQCTmVpG0ole-9zzVC80',
+    authDomain: 'aiook-ang.firebaseapp.com',
+    messagingSenderId: '182774739783',
+  },
+  production: true
+};
